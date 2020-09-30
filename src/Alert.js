d5992cc766e49ffc8a7ec85ac4b006df0c363cf0
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2020-10-01 00:33:28 +0300

    add(*): useReducer example

--- src/Alert.js
+++ src/Alert.js
@@ -2,11 +2,11 @@ import React, {useContext} from 'react';
 import { useAlert } from './AlertContext';
 
 export default function Alert() {
-	const {visible, toggle} = useAlert();
+	const {visible, hide, text} = useAlert();
 	if(!visible) return null;
 	return (
-		<div className="alert alert-danger" onClick={toggle}>
-			It's a very important message!
+		<div className="alert alert-danger" onClick={hide}>
+			{text}
 		</div>
 	)
 }
