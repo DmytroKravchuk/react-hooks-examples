b1993ea2391d59153cebc88dae3da1df8680a6f4
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2020-10-01 00:57:17 +0300

    add(*):  example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Main from './Main';
 import Alert from './Alert';
 import { AlertProvider } from './AlertContext';
 
+function useLogger(value) {
+  useEffect(() => {
+    console.log(value)
+  },[value])
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = e => {
+    setValue(e.target.value)
+  }
+  const clear = () => setValue('')
+  return {
+    bind: {value, onChange},
+    value,
+    clear
+  }
+}
+
 function App() {
+  const input = useInput('')
+  const lastName = useInput('')
+  useLogger(input.value)
+  useLogger(lastName.value)
   return (
-      <AlertProvider>
-        <div className="container pt-3">
-          <Alert/>
-          <Main/>
-        </div>
-      </AlertProvider>
+      <div className='container pt-3'>
+        <input type="text" {...input.bind}/>
+        <input type="text" {...lastName.bind}/>
+        <button className="btn btn-warning" onClick={() => {
+          input.clear()
+          lastName.clear()
+        }}>Clear</button>
+        <hr></hr>
+        {input.value}
+        {lastName.value}
+      </div>
   );
 }
 
